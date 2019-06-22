af3d2593d7a4f5458ead01b075342d73cf7de7ef
Ronalmoo <ydaniel0826@gmail.com>
2019-06-22 09:20:34 +0900

    # numeral system

---
+++ numeral_system.py
@@ -0,0 +1,27 @@
+a = 0xa
+print(a)
+print(bin(a), '\n')
+
+b = 0xb
+print(b)
+print(bin(b), '\n')
+
+c = 0xc
+print(c)
+print(bin(c), '\n')
+
+d = 0xd
+print(d)
+print(bin(d), '\n')
+
+e = 0xe
+print(e)
+print(bin(e), '\n')
+
+f = 0xf
+print(f)
+print(bin(f), '\n')
+
+address_8bit = 0b00101101
+# 16진수로 나타냄
+hex(address_8bit)
